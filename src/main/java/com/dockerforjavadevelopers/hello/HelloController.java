734a5f6acba114d6479a0afc06e793654631143a
Soumya06in <53549658+Soumya06in@users.noreply.github.com>
2020-08-21 13:50:01 +0530

    Update HelloController.java

--- src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
+++ src/main/java/com/dockerforjavadevelopers/hello/HelloController.java
@@ -10,7 +10,7 @@ public class HelloController {
     @RequestMapping("/")
     public String index() {
         //return "I am using SSH keys for deploying images from Jenkins in Kubernetes cluster!!!!\n";
-        System.out.println("I am using Github Webhook for Jenkins-Jira integration !!!");
+        System.out.println("I am using Github Webhook for Jenkins-Jira integration!!!");
     }
     
 }
